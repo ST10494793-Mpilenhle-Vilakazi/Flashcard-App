74d54f6f9026214dc322a8a50f1196cee9e77e95
ST10494793-Mpilenhle-Vilakazi <ST10494793@vcconnect.edu.za>
2025-05-20 02:43:32 +0200

    commit

---
+++ app/src/main/java/vcmsa/ci/flashcard_app/FlashcardActivity.kt
@@ -0,0 +1,77 @@
+package vcmsa.ci.flashcard_app
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class FlashcardActivity : AppCompatActivity() {
+    private var score = 0
+    private var currentQuestion = 0
+
+    //Sample Questions and answers
+    private val questions = listOf(
+        "The Earth is flat.",
+        "The moon is a planet.",
+        "The sun is smaller than the Earth.",
+        "Jupiter has 27 moons.",
+    )
+     private val answers = listOf(
+         false,
+         true,
+         false
+     )
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+      super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flashcard)
+
+    val questionText = findViewById<TextView>(R.id.questionTextView)
+    val trueButton = findViewById<Button>(R.id.trueButton)
+    val falseButton = findViewById<Button>(R.id.falseButton)
+    val feedbackText = findViewById<TextView>(R.id.feedbackTextView)
+    val nextButton = findViewById<Button>(R.id.nextButton)
+
+    // SHow first question
+questionText.text = questions[currentQuestion]
+
+    trueButton.setOnClickListener {
+        checkAnswer(true, feedbackText, trueButton, falseButton)
+    }
+    falseButton.setOnClickListener {
+        checkAnswer(false, feedbackText, trueButton, falseButton)
+    }
+    nextButton.setOnClickListener {
+        currentQuestion++
+
+        if(currentQuestion < questions.size) {
+
+            //Next question
+            questionText.text = questions[currentQuestion]
+            feedbackText.text = ""
+            trueButton.isEnabled = true
+            falseButton.isEnabled = true
+
+            //Quiz complete go to ScoreActivity
+            val intent = Intent(this, ScoreActivity::class.java)
+            intent.putExtra("SCORE", score)
+            intent.putExtra("TOTAL", questions.size)
+            startActivity(intent)
+            finish()
+
+        }
+    }
+}
+@SuppressLint("SetTextI18n")
+private fun checkAnswer(userAnswer: Boolean, feedbackText: TextView, trueButton: Button, falseButton: Button) {
+    if (userAnswer == answers[currentQuestion]) {
+        score++
+    } else {
+        feedbackText.text = "Wrong!"
+    }
+    trueButton.isEnabled = false
+    falseButton.isEnabled = false
+    }
+}
---
+++ app/src/main/java/vcmsa/ci/flashcard_app/ScoreActivity.kt
@@ -0,0 +1,22 @@
+package vcmsa.ci.flashcard_app
+
+import android.annotation.SuppressLint
+import vcmsa.ci.flashcard_app.R
+import vcmsa.ci.flashcard_app.R.id.scoreTextView
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+
+class ScoreActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_score)
+
+        val score = intent.getIntExtra("SCORE", 0)
+        val total = intent.getIntExtra("TOTAL", 0)
+
+        val scoreText = findViewById<TextView>(R.id.scoreTextView)
+        scoreText.text = "You scored $score out of $total"
+    }
+}
